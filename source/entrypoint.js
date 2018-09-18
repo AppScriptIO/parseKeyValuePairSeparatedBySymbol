ffde87bb576bc5f5417a5895e17d7ecdd4856405
Dr. Safi <sfmissive@gmail.com>
2018-09-18 06:20:00 +0300

    ✒  add transpilation module.

--- source/entrypoint.js
+++ source/entrypoint.js
@@ -1,27 +1,4 @@
-// 'x=y' --> { x: y }
-export function parseKeyValuePairSeparatedBySymbolConcatenatedString({ string, delimiter = ' ', separatingSymbol = '=' }) {
-    
-    return parseKeyValuePairSeparatedBySymbolFromArray({ 
-        array: string.split(separatingSymbol)
-    })
-}
+// Transpilation - babelJSCompiler
+require('@dependency/javascriptTranspilation')({ babelConfigurationFile: 'serverRuntime.BabelConfig.js' })
 
-// ['x=y'] --> { x: y }
-export function parseKeyValuePairSeparatedBySymbolFromArray({ array, separatingSymbol = '=' }) {
-    let parsedObject = {}
-    array.forEach( pair => {
-        let array = pair.split(separatingSymbol);
-        array[1] && (parsedObject[array[0]] = array[1]);
-    })
-    return parsedObject
-}
-
-
-export function combineKeyValueObjectIntoString({ object, delimiter = ' ', separatingSymbol='=' }) {
-    return Object.keys(object)
-        .reduce((previous, key) => {
-            previous.push(`${key}${separatingSymbol}${object[key]}`)
-            return previous;
-        }, [])
-        .join(delimiter)
-} 
+module.exports = require('./module.js')
