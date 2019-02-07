ec7d5275c73a0c3c10e9eee5078c09d226ed97df
Dr. Safi <sfmissive@gmail.com>
2019-02-07 14:26:55 +0200

    🐛 Fix entrypoint script name.

--- source/entrypoint.js
+++ source/entrypoint.js
@@ -1,4 +1,4 @@
 // Transpilation - babelJSCompiler
 require('@dependency/javascriptTranspilation')({ babelConfigurationFile: 'serverRuntime.BabelConfig.js' })
 
-module.exports = require('./module.js')
+module.exports = require('./script.js')
